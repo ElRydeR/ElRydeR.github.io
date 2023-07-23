e1c0b3d5fd5e1787b3250170e5305a42d1255c1e
ElRydeR <138314860+ElRydeR@users.noreply.github.com>
2023-07-23 17:22:44 -0400

    Add files via upload

---
+++ shorts_js.js
@@ -0,0 +1,60 @@
+document.getElementById("shorts").addEventListener("click", function() {
+	window.location.href = "shorts.html";
+  });
+document.getElementById("licra_tops").addEventListener("click", function() {
+	window.location.href = "licra_tops.html";
+  });
+document.getElementById("blumers").addEventListener("click", function() {
+  window.location.href = "blumers.html";
+  });
+document.getElementById("medias").addEventListener("click", function() {
+  window.location.href = "medias.html";
+  });
+document.getElementById("ajustadores").addEventListener("click", function() {
+  window.location.href = "ajustadores.html";
+  });
+document.getElementById("licras").addEventListener("click", function() {
+  window.location.href = "licras.html";
+  });
+document.getElementById("cremas").addEventListener("click", function() {
+  window.location.href = "cremas.html";
+  });
+document.getElementById("pantalones").addEventListener("click", function() {
+  window.location.href = "pantalones.html";
+  });
+document.getElementById("tintes").addEventListener("click", function() {
+  window.location.href = "tintes.html";
+  });
+document.getElementById("trusas").addEventListener("click", function() {
+  window.location.href = "trusas.html";
+  });
+document.getElementById("sayas").addEventListener("click", function() {
+  window.location.href = "sayas.html";
+  });
+document.getElementById("blusas").addEventListener("click", function() {
+  window.location.href = "blusas.html";
+  });
+document.getElementById("colonias").addEventListener("click", function() {
+  window.location.href = "colonias.html";
+  });
+document.getElementById("carteras").addEventListener("click", function() {
+  window.location.href = "carteras.html";
+  });
+document.getElementById("aretes").addEventListener("click", function() {
+  window.location.href = "aretes.html";
+  });
+document.getElementById("calzoncillos").addEventListener("click", function() {
+  window.location.href = "calzoncillos.html";
+  });
+document.getElementById("audifonos").addEventListener("click", function() {
+  window.location.href = "audifonos.html";
+  });
+document.getElementById("pomos").addEventListener("click", function() {
+  window.location.href = "pomos.html";
+  });
+document.getElementById("zapatos").addEventListener("click", function() {
+  window.location.href = "zapatos.html";
+  });  
+  document.getElementById("vestidos").addEventListener("click", function() {
+    window.location.href = "vestidos.html";
+    });  
